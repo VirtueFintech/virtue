bc1c1221d7f1f05a05a5938404afe14b68c98590
Hisham Ismail <mhi@virtue.finance>
2017-12-22 19:23:30 +0800

    Added time server for synch between nodes

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@ PROJECT_VERSION = 0.1.0
 SP = 2
 
 DEPS = lager sync
-LOCAL_DEPS = vdag vnet vkey vdata mnesia crypto public_key inets sasl
+LOCAL_DEPS = vdag vnet vkey vdata vtime \
+						 mnesia crypto public_key inets sasl
 
 include erlang.mk
--- apps/vtime/src/vtime_server.erl
+++ apps/vtime/src/vtime_server.erl
@@ -2,7 +2,8 @@
 -behaviour(gen_server).
 
 %% API.
--export([start_link/0]).
+-export ([start_link/0]).
+-export ([time/0]).
 
 %% gen_server.
 -export([init/1]).
@@ -30,10 +31,12 @@ time() ->
 %% gen_server.
 
 init([]) ->
-  % fetch time from time server, and added delta here.
+  % TODO: fetch time from time server, and added delta here.
 	{ok, #state{delta=0}}.
 
-handle_call({time}, _From, State) ->
+handle_call({time}, _From, State = #state{delta=Delta}) ->
+  Time = erlang:universaltime_to_posixtime(erlang:universaltime()),
+  {reply, {ok, Time + Delta}, State};
   
 handle_call(_Request, _From, State) ->
 	{reply, ignored, State}.
